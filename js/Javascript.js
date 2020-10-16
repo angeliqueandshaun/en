172e810ea471a44b6f2125cbb84e3c36009313d3
ShaunTrusler <68854165+ShaunTrusler@users.noreply.github.com>
2020-10-16 17:40:48 +0200

    update

--- js/Javascript.js
+++ js/Javascript.js
@@ -1,5 +1,42 @@
 var res = new Array;
 var bookingArray = new Array;
+			
+			
+			function setCookie() {
+			  var d = new Date();
+			  d.setTime(d.getTime() + 10000);
+			  var expires = "expires=" + d.toGMTString();
+			  document.cookie = "username" + "=" + "shaun" + ";" + expires + ";path=/";
+			  //document.getElementById('Language-Modal').style.display = 'none';
+			}
+			
+			function getCookie() {
+			  var name = "username" + "=";
+			  var decodedCookie = decodeURIComponent(document.cookie);
+			  var ca = decodedCookie.split(';');
+			  for(var i = 0; i < ca.length; i++) {
+				var c = ca[i];
+				while (c.charAt(0) == ' ') {
+				  c = c.substring(1);
+				}
+				if (c.indexOf(name) == 0) {
+				  return c.substring(name.length, c.length);
+				}
+			  }
+			  return "";
+			}
+
+			function checkCookie() {
+			  var user = getCookie();
+			  if (user != "") {
+				alert('cookie');				
+				document.getElementById('Language-Modal').style.display = 'none';
+				
+			  }else{
+				setCookie();
+				alert('no cookie');
+			  }
+			}
 
 function ScrollTop(){				
 	window.scrollTo(0,0);
